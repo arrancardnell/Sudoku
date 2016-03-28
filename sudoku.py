d7062dec5952a6966ea50dadce566af6dd88f9d4
arrancardnell <arrancardnell@gmail.com>
2016-03-28 20:08:58 +0100

    Added the UI and puzzle methods

--- sudoku.py
+++ sudoku.py
@@ -103,3 +103,53 @@ class SudokuGame(object):
                 for c in xrange(column * 3, (column + 1) * 3)
             ]
         )
+
+class SudokuUI(Frame):
+    """
+    The Tkinter UI, responsible for drawing the board and accepting user input.
+    """
+    def __init__(self, parent, game):
+        self.game = game
+        self.parent = parent
+        Frame.__init__(self, parent)
+
+        self.row, self.col = 0, 0
+
+        self.__initUI()
+
+    def __initUI(self):
+        self.parent.title("Sudoku")
+        self.pack(fill=BOTH, expand=1)
+        self.canvas = Canvas(self,
+                             width=WIDTH,
+                             height=HEIGHT)
+        self.canvas.pack(fill=BOTH, side=TOP)
+        clear_button = Button(self,
+                              text="Clear answers",
+                              command=self.__clear_answers)
+        clear_button.pack(fill=BOTH, side=BOTTOM)
+
+        self.__draw_grid()
+        self.__draw_puzzle()
+
+        self.canvas.bind("<Button-1", self.__cell_clicked)
+        self.canvas.bind("<Key", self.__key_pressed)
+
+    def __draw_grid(self):
+        """
+        Draws grid divided with blue lines into 3x3 squares.
+        """
+        for i in xrange(10):
+            color = "blue" if i % 3 == 0 else "gray"
+
+            x0 = MARGIN + i * SIDE
+            y0 = MARGIN
+            x1 = MARGIN + i * SIDE
+            y1 = HEIGHT - MARGIN
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
+
+            x0 = MARGIN
+            y0 = MARGIN + i * SIDE
+            x1 = WIDTH - MARGIN
+            y1 = MARGIN + i * SIDE
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
